0ce6f433652ae488ddb6634b42445dd090902c3b
Robin van Leeuwen <robinvanleeuwen@gmail.com>
2017-09-20 12:58:11 +0200

    Replace the commonName in openssl.cnf

--- update_all_domains.sh
+++ update_all_domains.sh
@@ -1,11 +1,17 @@
 #!/usr/local/bin/bash
 
+echo "Last Run $(date)" > /var/log/update_ssl_certificates.log
 
 source /usr/local/etc/dehydrated/config
 
-for i in `cat $DOMAINS_TXT`;do 
+cp $OPENSSL_CNF /tmp/openssl.org
 
+for i in `cat $DOMAINS_TXT`; do 
+   sed -e "/\[ req_distinguished_name \]/,/^\[/s/commonName                      = .*/commonName                      = $i/" $OPENSSL_CNF > /tmp/tmp_openssl.cnf
+   mv /tmp/tmp_openssl.cnf $OPENSSL_CNF
    /opt/dehydrated/dehydrated -c -d $i
 
 done
+
+cp /tmp/openssl.org $OPENSSL_CNF
 
