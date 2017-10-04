74d03267753bb2e747e52fb70e8f8f57bf1a84cd
Robin van Leeuwen <robinvanleeuwen@gmail.com>
2017-10-04 10:16:51 +0200

    Renew a single domain

---
+++ renew_single_certificate.sh
@@ -0,0 +1,26 @@
+#!/usr/local/bin/bash
+echo "Last Run $(date)" > /var/log/replace_single_certificate.log
+
+source /usr/local/etc/dehydrated/config
+
+if [ "$1" == "" ]; then
+    echo "Usage: $0 <domain>"
+    exit
+fi
+
+echo " - Backup up $OPENSSL_CNF"
+cp $OPENSSL_CNF /tmp/openssl.org
+
+echo " - Replacing domain in new OpenSSL config"
+sed -e "/\[ req_distinguished_name \]/,/^\[/s/commonName                      = .*/commonName                      = $1/" $OPENSSL_CNF > /tmp/tmp_openssl.cnf
+
+echo " - Updating $OPENSSL_CNF"
+mv /tmp/tmp_openssl.cnf $OPENSSL_CNF
+
+/opt/dehydrated/dehydrated -c -d $1
+
+echo " - Restoring backupped $OPENSSL_CNF"
+cp /tmp/openssl.org $OPENSSL_CNF
+
+
+
