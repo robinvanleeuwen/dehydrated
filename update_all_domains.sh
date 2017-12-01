fac022c96db74f845f20a010becdd8e3c4b153a2
Robin van Leeuwen <robinvanleeuwen@gmail.com>
2017-12-01 09:24:18 +0100

    Update sed action

--- update_all_domains.sh
+++ update_all_domains.sh
@@ -7,7 +7,7 @@ source /usr/local/etc/dehydrated/config
 cp $OPENSSL_CNF /tmp/openssl.org
 
 for i in `cat $DOMAINS_TXT`; do 
-   sed -e "/\[ req_distinguished_name \]/,/^\[/s/commonName                      = .*/commonName                      = $i/" $OPENSSL_CNF > /tmp/tmp_openssl.cnf
+   sed -e "/\[ req_distinguished_name \]/,/^\[/s/commonName = .*/commonName                      = $i/" $OPENSSL_CNF > /tmp/tmp_openssl.cnf
    mv /tmp/tmp_openssl.cnf $OPENSSL_CNF
    /opt/dehydrated/dehydrated -c -d $i
 
