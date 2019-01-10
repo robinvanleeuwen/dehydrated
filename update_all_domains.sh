cfd160a6e5697d34e1df3f744c01af409d519c7b
Robin van Leeuwen <robinvanleeuwen@gmail.com>
2019-01-10 10:06:31 +0100

    Add logging

--- update_all_domains.sh
+++ update_all_domains.sh
@@ -1,17 +1,23 @@
 #!/usr/local/bin/bash
 
-echo "Last Run $(date)" > /var/log/update_ssl_certificates.log
-
+# Moved to overlying script that runs both update certs and renew in pfsense
+#echo "Last Run $(date)" >> /var/log/update_ssl_certificates.log
+echo "Starting script."
+echo "Sourcing config file"
 source /usr/local/etc/dehydrated/config
-
+echo "Copying $OPENSSL_CNF"
 cp $OPENSSL_CNF /tmp/openssl.org
 
+echo "Running for loop"
 for i in `cat $DOMAINS_TXT`; do 
+   echo -n ". $i"
    sed -e "/\[ req_distinguished_name \]/,/^\[/s/commonName[[:blank:]]*=[[:blank:]].*/commonName = $i/" $OPENSSL_CNF > /tmp/tmp_openssl.cnf
    mv /tmp/tmp_openssl.cnf $OPENSSL_CNF
-   /opt/dehydrated/dehydrated -c -d $i
-
+   echo -n "x"
+   /opt/dehydrated/dehydrated -c -d $i >> /var/log/update_ssl_certificates.log
 done
-
+echo "Done fore loop"
+echo "Copying original '$OPENSSL_CNF' back"
 cp /tmp/openssl.org $OPENSSL_CNF
+echo "Done script"
 
